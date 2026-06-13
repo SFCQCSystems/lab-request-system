ebec1ec06fddc7e4bc828171cf29beabc65441ef
scottist <guszio.1211@gmail.com>
2026-06-13 21:18:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
   const defaultConfig = {
     dbMode: 'supabase', // Force supabase
     supabaseUrl: 'https://vldpwwqnskpkgqdpsecu.supabase.co',
-    supabaseAnonKey: 'sb_publishable_5S1e8WrTTVzHIpmgOfElUQ_HRQrSaYO'
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsZHB3d3Fuc2twa2dxZHBzZWN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MTYyNTIsImV4cCI6MjA5NjI5MjI1Mn0.JrUe8TSFt4qJzqNCGv4xtyXJrpGE_PhcYIrZA1aPEw0'
   };
 
   window.AppConfig = {
